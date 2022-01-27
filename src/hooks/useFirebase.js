c5d9d337fde738b5621a3488168fac9ef0cd5480
jaber4770 <jaber.ahmed4770@gmail.com>
2022-01-27 20:08:27 +0600

    login and sign in hooks are added

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+    const [error, setError] = useState({});
+
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+    const SignInUsingGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                setUser(result.user);
+                console.log(result.user);
+            })
+            .catch(error => {
+                setError(error.message);
+                console.log(error.message);
+            })
+
+    }
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                setUser(user);
+            }
+        })
+    }, [])
+
+    const logOut = () => {
+        signOut(auth)
+            .then(() => {
+                setUser({});
+            })
+    }
+
+    return {
+        user,
+        error,
+        logOut,
+        SignInUsingGoogle
+    }
+
+
+}
+
+export default useFirebase;
